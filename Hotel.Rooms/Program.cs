59f717589b81f9d8edf332aa10f5f5efbe2e05fc
cesar8389 <cesar8389@outlook.com>
2019-07-18 15:23:55 -0300

    Beta

---
+++ Hotel.Rooms/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Hotel.Rooms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
